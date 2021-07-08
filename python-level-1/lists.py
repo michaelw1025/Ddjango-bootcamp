d2429cc4dc85a89f79833258cefb59f8df8a0b21
michaelw1025 <michaelw1025@gmail.com>
2021-07-08 02:01:38 -0500

    cont

---
+++ python-level-1/lists.py
@@ -0,0 +1,8 @@
+# mylist = [1,2,3]
+# mylist = ['string', 1, 2, 3, 23.2, True, 'asdf', [1,2,3]]
+# print(mylist)
+# print(len(mylist))
+
+mylist = ['a', 'b', 'c', 'd', 'e']
+# print(mylist[0])
+print(mylist[1:])
---
+++ python-level-1/strings.py
@@ -0,0 +1,21 @@
+# Basics
+# 'hello'
+# "hello"
+
+# Indexing
+# mystring = 'abcdefg'
+# print(mystring[3])
+
+# Slicing
+mystring = 'Hello World'
+# print(mystring[4:])
+# print(mystring[:3])
+# print(mystring[2:5])
+
+# Methods
+# print(mystring.upper())
+# print(mystring.split('e'))
+
+# Print Formatting
+# print("Item one: {}, Item two: {}".format("dog", "cat"))
+print("Item one: {y}, Item two: {x}".format(x = "dog", y =  "cat"))
